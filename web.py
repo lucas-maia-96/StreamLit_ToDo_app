3ee3e9ba824c30ae05b1f1324fa6bfc2af659c93
Lucas Maia <lucasmaia@dcc.ufmg.br>
2023-07-25 22:54:29 -0300

    Add and Complete functions Working

--- web.py
+++ web.py
@@ -4,21 +4,23 @@ import functions
 todos = functions.get_todos()
 
 def add_todo():
-    todo = st.session_state['new_todo'] + "\n"
-    todos.append(todo)
-    functions.write_todos(todos)
-    
-
-
+    todo_local = st.session_state["new_todo"]
+    if todo_local not in todos and todo_local:
+        todos.append(todo_local + '\n')
+        functions.write_todos(todos)
+        st.session_state["new_todo"] = ""
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your produtivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    check_box = st.checkbox(todo, key=todo)
+    if check_box:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
     
 st.text_input(label="New:", placeholder="Add new todo..", 
               on_change=add_todo, key='new_todo') 
-
-st.session_state
